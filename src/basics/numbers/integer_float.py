211e943e96162c1624c04ac3f0d219da588ac7e3
ylongtay <145927097+ylongtay@users.noreply.github.com>
2026-07-28 00:00:39 +0800

    Python Crash Course Ex 2: Numbers and challenges

---
+++ src/basics/numbers/integer_float.py
@@ -0,0 +1,21 @@
+## Python Crash Course 3rd ed - Exercises 2"
+
+# Ex 2-9 Number 8: Write addition, subtraction, multiplication, and division
+# operations that each result in the number 8
+
+print(1 + 7)
+print(12 - 4)
+print(4 * 2)
+# Divide any two numbers will always get a float even if 
+# they are integers that result in a whole number
+print(16 / 2)
+
+# Ex 2-10 Favorite Number
+
+my_fav_number = 101
+message = f"My favorite number is {my_fav_number}."
+print(message)
+
+# Ex 2-11 Adding comment: Add comment to at least one of your program
+answer = "Comment already added in this program. See above # lines"
+print(answer)
---
+++ src/challenges/pcc_ch2/receipt_cleaner.py
@@ -0,0 +1,22 @@
+# Challenge 1: The E-Commerce Receipt & Cleaner
+# Scenario: You are building a backend script for a retail store. 
+# The system received raw customer input with messy spacing and capitalization, 
+# alongside an internal product URL.
+
+customer_name_entered = "   jAnE dOe   "
+product_url = "https://mystore.com/products/bluetooth_headsets"
+item_price = 79.99
+quantity = 3
+
+# Task 1: Clean up the customer's name by stripping and change to title case
+customer_name_clean = customer_name_entered.lower().title().strip()
+
+# Task 2: Extract the clean product name from the URL by removing prefix
+product_name = product_url.removeprefix("https://mystore.com/products/")
+
+# Task 3: Calculate the total cost before tax and with a 9 % GST included
+subtotal = item_price * quantity
+total_cost = subtotal * 1.09
+
+# Task 4: Print a formatted receipt
+print(f"=== OFFICIAL RECEIPT === \nCustomer:\t{customer_name_clean}\nItems:\t{product_name}\nQuantity:\t{quantity}\nSubtotal:\t{subtotal}\nTotal (incl.GST):\t{total_cost}")
