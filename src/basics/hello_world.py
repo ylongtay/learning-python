c5e55c30f1324c35e0bc1d8af7513679ef2cfd81
ylongtay <145927097+ylongtay@users.noreply.github.com>
2026-07-27 18:27:44 +0800

    Python Crash Course Ex 2: Variable and Strings

---
+++ src/basics/hello_world.py
@@ -0,0 +1,21 @@
+## Python Crash Course 3rd Edition ##
+
+#long-held tradition in the programming world
+#printing the message Hello world! to the screen as your first program
+#in a new language will bring you good luck.
+print("Hello Python world!")
+
+## Practice 2-1: Simple Messages
+
+# Assign a message to a variable, and then print that message
+message = "Hello Python!!"
+print(message)
+
+# change the value of the variable to a new message, and print the new message.
+message = "Hello Python Crash Course world!"
+print(message)
+
+# Variables name matches, Python see no issue
+# Programming languages are strict, but they disregard good and bad spelling
+mesage = "Hello Python Course Reader!"
+print(mesage)
